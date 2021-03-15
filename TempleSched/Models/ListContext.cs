939c45e1d2fc335e0a8054769ce7cc515fe27cc9
benjam-rich <70960034+benjam-rich@users.noreply.github.com>
2021-03-15 17:53:42 -0600

    Database setup needed

---
+++ TempleSched/Models/ListContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TempleSched.Models
+{
+    public class ListContext : DbContext
+    {
+        public ListContext(DbContextOptions<ListContext> options): base (options)
+        { }
+
+        public DbSet<Group> Groups { get; set; }
+    }
+}
